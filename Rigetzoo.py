972a7b70188ff40df4b1e680e9e027c9e94531d0
Cos-A22 <costantinosantonio13@gmail.com>
2026-02-10 23:23:15 +0000

    added all html templates, made a base template with the nav bar for all templates

---
+++ Rigetzoo.py
@@ -0,0 +1,28 @@
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/hotel')
+def hotel():
+    return render_template('hotel.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
